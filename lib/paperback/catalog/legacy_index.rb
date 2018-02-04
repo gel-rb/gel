0ccdc54ea5fc4d9031c52f7f7369bd4908e1a9ed
Matthew Draper <matthew@trebex.net>
2018-02-04 23:07:14 +1030

    Hacks are nasty, but let's give them their own file

--- lib/paperback/catalog/legacy_index.rb
+++ lib/paperback/catalog/legacy_index.rb
@@ -8,21 +8,7 @@ require "zlib"
 
 require_relative "../pinboard"
 
-class Gem::Dependency
-  attr_accessor :name, :type, :requirement
-end
-class Gem::Specification
-  attr_accessor :required_ruby_version
-  attr_accessor :dependencies
-
-  def self._load(str)
-    array = Marshal.load(str)
-    o = new
-    o.required_ruby_version = array[6].as_list
-    o.dependencies = array[9].map { |d| [d.name, d.requirement.as_list] if d.type == :runtime }.compact
-    o
-  end
-end
+require_relative "marshal_hacks"
 
 class Paperback::Catalog::LegacyIndex
   UPDATE_CONCURRENCY = 8
---
+++ lib/paperback/catalog/marshal_hacks.rb
@@ -0,0 +1,15 @@
+class Gem::Dependency
+  attr_accessor :name, :type, :requirement
+end
+class Gem::Specification
+  attr_accessor :required_ruby_version
+  attr_accessor :dependencies
+
+  def self._load(str)
+    array = Marshal.load(str)
+    o = new
+    o.required_ruby_version = array[6].as_list
+    o.dependencies = array[9].map { |d| [d.name, d.requirement.as_list] if d.type == :runtime }.compact
+    o
+  end
+end
