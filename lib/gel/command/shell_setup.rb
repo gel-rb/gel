839e6cdea54cb50ae3aee0c92e841399da6ad591
Matthew Draper <matthew@trebex.net>
2019-04-12 23:38:47 +0930

    Remember to export variables that we've changed
    PATH will already be exported, but we might as well include it for
    consistency.

--- lib/gel/command/shell_setup.rb
+++ lib/gel/command/shell_setup.rb
@@ -4,14 +4,22 @@ class Gel::Command::ShellSetup < Gel::Command
   def run(command_line)
     require "shellwords"
 
+    variables = []
+
     bin_dir = File.expand_path("~/.local/gel/bin")
     unless ENV.fetch("PATH", "").split(File::PATH_SEPARATOR).include?(bin_dir)
       puts "PATH=\"#{Shellwords.shellescape bin_dir}#{File::PATH_SEPARATOR}$PATH\""
+      variables << "PATH"
     end
 
     lib_dir = File.expand_path("../compatibility", __dir__)
     unless ENV.fetch("RUBYLIB", "").split(File::PATH_SEPARATOR).include?(lib_dir)
       puts "RUBYLIB=\"#{Shellwords.shellescape lib_dir}:$RUBYLIB\""
+      variables << "RUBYLIB"
+    end
+
+    unless variables.empty?
+      puts "export #{variables.join(" ")}"
     end
   end
 end
