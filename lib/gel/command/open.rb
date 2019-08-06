9d5496233d6b0b1ca38bc6c236700a1d9ee5aace
Samuel Cochran <sj26@sj26.com>
2019-08-06 12:30:07 +1000

    Give a name a name

--- lib/gel/command/open.rb
+++ lib/gel/command/open.rb
@@ -6,14 +6,15 @@ class Gel::Command::Open < Gel::Command
   def run(command_line)
     raise "Please provide the name of a gem to open in your editor" if command_line.empty?
     raise "Too many arguments, only 1 gem name is supported" if command_line.length > 1
+    gem_name = command_line.shift
 
     editor = ENV.fetch("GEL_EDITOR", ENV["EDITOR"])
     raise "An editor must be set using either $GEL_EDITOR or $EDITOR" unless editor
 
     Gel::Environment.activate(output: $stderr)
 
-    found_gem = Gel::Environment.find_gem(command_line.first)
-    raise "Can't find gem `#{command_line.first}`" unless found_gem
+    found_gem = Gel::Environment.find_gem(gem_name)
+    raise "Can't find gem `#{gem_name}`" unless found_gem
 
     command = [*Shellwords.split(editor), found_gem.root]
     exec(*command)
