e529e5e629258e78434237ce6b2758df4b5a9556
Samuel Cochran <sj26@sj26.com>
2019-08-29 09:35:39 +1000

    Gel::DB::File test

--- test/db_test.rb
+++ test/db_test.rb
@@ -31,4 +31,32 @@ class DbTest < Minitest::Test
       assert_equal db['test'], hash_string
     end
   end
+
+  def test_file_store_value_retrieval
+    Dir.mktmpdir do |dir|
+      db = Gel::DB::File.new(dir, 'test-store')
+
+      # Still handles ints okay
+      db['test'] = 12
+      assert_equal db['test'], 12
+
+      # A simple string should return a simple string
+      db['test'] = 'Hello World'
+      assert_equal db['test'], 'Hello World'
+
+      # A simple hash should return a simple hash
+      db['test'] = { a: 'Hello World' }
+      assert_equal db['test'], { a: 'Hello World' }
+
+      # A long string
+      long_string = '*' * 1024
+      db['test'] = long_string
+      assert_equal db['test'], long_string
+
+      # A object with a long string gets stored and marshaled correctly
+      hash_string = { a: long_string, b: long_string }
+      db['test'] = hash_string
+      assert_equal db['test'], hash_string
+    end
+  end
 end
