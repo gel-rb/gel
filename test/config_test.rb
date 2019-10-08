855581f9373838faac54825978eab690df38f946
Haroon Ahmed <haroon.ahmed25@gmail.com>
2019-10-08 09:15:51 +0100

    cleanup code by moving env stuff into setup and teardown

--- test/config_test.rb
+++ test/config_test.rb
@@ -3,37 +3,34 @@
 require "test_helper"
 
 class ConfigTest < Minitest::Test
-  def test_reading_key_from_config_successully
-    prev_gel_config = ENV['GEL_CONFIG']
+  def setup
+    @old_gel_config_env = ENV['GEL_CONFIG']
     ENV['GEL_CONFIG'] = fixture_file('')
+  end
+
+  def teardown
+    ENV['GEL_CONFIG'] = @old_gel_config_env
+  end
+
+  def test_reading_key_from_config_successully
     klass = Gel::Config.new
     assert_equal '316429e', klass['packages.example.io']
-    ENV['GEL_CONFIG'] = prev_gel_config
   end
 
   def test_reading_group_key_from_config_successully
-    prev_gel_config = ENV['GEL_CONFIG']
-    ENV['GEL_CONFIG'] = fixture_file('')
     klass = Gel::Config.new
     assert_equal '316429e', klass['packages', 'example.io']
-    ENV['GEL_CONFIG'] = prev_gel_config
   end
 
   def test_reading_uppercase_key_from_config_successully
-    prev_gel_config = ENV['GEL_CONFIG']
-    ENV['GEL_CONFIG'] = fixture_file('')
     klass = Gel::Config.new
     assert_equal 'true', klass['gem.MIT']
-    ENV['GEL_CONFIG'] = prev_gel_config
   end
 
   def test_reading_all_from_config_successully
-    prev_gel_config = ENV['GEL_CONFIG']
-    ENV['GEL_CONFIG'] = fixture_file('')
     settings = Gel::Config.new.all
     assert_equal '316429e', settings['packages.example.io']
     assert_equal 'true', settings['gem.mit']
     assert_equal '10', settings['timeout']
-    ENV['GEL_CONFIG'] = prev_gel_config
   end
 end
