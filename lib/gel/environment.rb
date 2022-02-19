cba1207575b39bcf2c2d28b0a87b5931fb00a39e
Matthew Draper <matthew@trebex.net>
2022-02-19 22:37:08 +1030

    Plan gems needed to support a require all at once
    Previously we were activating them recursively on demand. We still
    eagerly choose dependencies (this is not a gemfile-style resolve), but
    this saves some double-handling, and potentially also reduces cache
    invalidations for $LOAD_PATH.

--- lib/gel/environment.rb
+++ lib/gel/environment.rb
@@ -63,7 +63,11 @@ class Gel::Environment
 
   def self.open(store)
     @store = store
-    gems_from_lock(store.locked_versions) if store.respond_to?(:locked_versions) && store.locked_versions
+
+    if store.respond_to?(:locked_versions) && store.locked_versions
+      gems = store.gems(store.locked_versions)
+      activate_gems gems.values
+    end
   end
 
   def self.original_rubylib
@@ -526,18 +530,6 @@ class Gel::Environment
     end
   end
 
-  def self.gems_from_lock(name_version_pairs)
-    gems = @store.gems(name_version_pairs)
-
-    dirs = []
-    gems.each do |name, g|
-      dirs += g.require_paths
-    end
-
-    activated_gems.update gems
-    $:.concat dirs
-  end
-
   def self.activate_gem(gem, why: nil)
     raise gem.version.class.name unless gem.version.class == String
     if activated_gems[gem.name]
@@ -551,11 +543,97 @@ class Gel::Environment
       self.gem(dep, *reqs.map { |(qual, ver)| "#{qual} #{ver}" }, why: ["required by #{gem.name} #{gem.version}", *why])
     end
 
-    lib_dirs = gem.require_paths
-    @store.prepare gem.name => gem.version
+    activate_gems [gem]
+  end
 
-    activated_gems[gem.name] = gem
+  def self.activate_gems(gems)
+    lib_dirs = gems.flat_map(&:require_paths)
+    preparation = {}
+    activation = {}
+
+    gems.each do |g|
+      preparation[g.name] = g.version
+      activation[g.name] = g
+    end
+
+    @store.prepare(preparation)
+
+    activated_gems.update(activation)
     $:.concat lib_dirs
+  end
+
+  # Returns either an array of compatible gems that must all be activated
+  # (in the specified order) to activate the given +gem+, or a string
+  # describing a dependency conflict that prevents it.
+  #
+  ##
+  #
+  # Recurses using internal +context+ as a hash of additional gems to
+  # consider already activated. This is used to identify internal conflicts
+  # between pending dependencies.
+  def self.gems_for_activation(gem, why: nil, context: {})
+    if active_gem = activated_gems[gem.name] || context[gem.name]
+      # This gem name is already active. Either it's the right version, and
+      # we have nothing to do, or it's the wrong version, and we're unable
+      # to proceed.
+      if active_gem == gem
+        return []
+      else
+        return "already activated #{gem.name} #{active_gem.version} (can't activate #{gem.version})"
+      end
+    end
+
+    context = context.dup
+    new_gems = [gem]
+    context[gem.name] = gem
+
+    gem.dependencies.each do |dep, reqs|
+      next if IGNORE_LIST.include?(dep)
+
+      inner_why = ["required by #{gem.name} #{gem.version}", *why]
+
+      requirements = Gel::Support::GemRequirement.new(
+        reqs.map { |(qual, ver)| "#{qual} #{ver}" }
+      )
+
+      if existing = activated_gems[dep] || context[dep]
+        if existing.satisfies?(requirements)
+          next
+        else
+          return "already loaded gem #{dep} #{existing.version}, which is incompatible with: #{requirements} (#{inner_why.join("; ")})"
+        end
+      end
+
+      resolved = nil
+      first_failure = nil
+
+      candidates = @store.each(dep).select do |g|
+        g.satisfies?(requirements)
+      end
+
+      candidates.each do |g|
+        result = gems_for_activation(g, why: inner_why, context: context)
+        if result.is_a?(String)
+          first_failure ||= result
+        else
+          resolved = result
+          break
+        end
+      end
+
+      if resolved
+        new_gems += resolved
+        resolved.each do |r|
+          context[r.name] = r
+        end
+      elsif first_failure
+        return first_failure
+      else
+        return "unable to satisfy requirements for gem #{dep}: #{requirements} (#{inner_why.join("; ")})"
+      end
+    end
+
+    new_gems
   end
 
   def self.gem_has_file?(gem_name, path)
@@ -580,33 +658,88 @@ class Gel::Environment
     end
   end
 
+  # Search gems for how we should load the given +path+
+  #
+  # Returns nil when the path is unrecognised (caller should fall back to
+  # scanning $LOAD_PATH). Otherwise, returns an array tuple:
+  #
+  # [
+  #   gem,        #
+  #   file,       # full path to require, or nil if gem is conflicted
+  #   resolved,   # array of gems to activate, or nil if empty
+  #               # if conflicted: string describing conflict
+  # ]
   def self.scan_for_path(path)
     if @store && !path.start_with?("/")
       search_name, search_ext = Gel::Util.split_filename_for_require(path)
 
-      results = []
+      # Fast scan first: find all the gems that supply a file matching
+      # +search_name+ (ignoring ext for now)
+      hits = []
       @store.gems_for_lib(search_name) do |gem, subdir, ext|
+        hits << [gem, subdir, ext]
+      end
+
+      # Now we get a bit more detailed: 1) skip any results that don't
+      # match the +search_ext+; 2) immediately return if we've matched a
+      # gem that's already loaded.
+      results = []
+      hits.each do |gem, subdir, ext|
         next unless Gel::Util.ext_matches_requested?(ext, search_ext)
 
-        results << [gem, subdir]
-        break if activated_gems[gem.name] == gem
+        if activated_gems[gem.name] == gem
+          return [gem, gem.path(path, subdir), nil]
+        else
+          results << [gem, subdir, ext]
+        end
       end
-      results.find { |g, _| activated_gems[g.name] == g } || results.first
+
+      # Okay, no already-loaded gems supply the file we're looking for.
+      # +results+ contains a list of gems that we could load.
+
+      # We're going to have to activate a gem if we can. Recursively plan
+      # out the set of dependencies we need to activate... or alternatively,
+      # identify the conflict that prevents it.
+      first_activation_error = nil
+      results.each do |gem, subdir, ext|
+        a = gems_for_activation(gem, why: ["provides #{path.inspect}"])
+        if a.is_a?(Array)
+          # This is a valid dependency set; activate +a+, and require the
+          # file.
+          return [gem, gem.path(path, subdir), a]
+        else
+          # If we don't find a better answer later in this loop, then this
+          # will be the failure we report.
+          first_activation_error ||= [gem, nil, a]
+        end
+      end
+
+      # Still no luck: this file cannot be resolved. If we found a gem that
+      # was blocked by a conflict, we'll return the explanation as a string.
+      # Otherwise (no installed gems have any knowledge of this file) we
+      # return nil.
+      first_activation_error
     end
   end
 
   def self.gem_for_path(path)
-    if result = scan_for_path(path)
-      result[0]
-    end
+    gem, _file, _resolved = scan_for_path(path)
+    gem
   end
 
   def self.resolve_gem_path(path)
     path = path.to_s # might be e.g. a Pathname
 
-    if result = scan_for_path(path)
-      activate_gem result[0], why: ["provides #{path.inspect}"]
-      return result[0].path(path, result[1])
+    gem, file, resolved = scan_for_path(path)
+
+    if file
+      if gem && resolved
+        activate_gems resolved
+      end
+
+      return file
+    elsif resolved
+      raise LoadError, resolved
     end
 
     path
